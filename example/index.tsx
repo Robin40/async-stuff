8c2c5f608b566c2aee25feb39b975d0502574775
Robinson Castro F <r.a.c.f.28@gmail.com>
2021-01-18 21:21:20 -0300

    ...

--- example/index.tsx
+++ example/index.tsx
@@ -5,39 +5,148 @@ import * as ReactDOM from 'react-dom';
 import { Thing } from '../.';
 import { Server } from '../src/Server';
 import {
-    any,
     array,
     boolean,
-    coerce,
-    define,
-    Describe,
-    Infer,
-    instance,
+    enums,
     integer,
+    lazy,
+    nullable,
     number,
     object,
+    optional,
     string,
+    union,
 } from 'superstruct';
-import { Decimal } from 'decimal.js';
-import { LocalDate } from 'js-joda';
-
-const decimal = () => coerce(instance(Decimal), string(), s => new Decimal(s));
-// const localDate = () => coerce(instance(LocalDate as any), string(), s => LocalDate.parse(s));
+import { decimal, localDate } from '../src/types';
 
 const server = new Server(
     'https://yfxit29sub.execute-api.us-west-1.amazonaws.com/dev/'
 );
 
-const morosidades = server.endpoint.getAll(
-    'documentos/morosidades',
-    array(
+const models = {
+    Divisa: () => enums(['pesos', 'dolares', 'uf']),
+
+    Morosidad: () =>
         object({
             monto: decimal(),
-            divisa: string(),
+            divisa: lazy(models.Divisa),
             pagado: boolean(),
             id: integer(),
-        })
-    )
+        }),
+
+    VoucherPagination: () =>
+        object({
+            count: integer(),
+            next: optional(nullable(string())),
+            previous: optional(nullable(string())),
+            results: array(lazy(models.Voucher)),
+        }),
+
+    Voucher: () =>
+        object({
+            id: integer(),
+            fecha: localDate(),
+            state: lazy(models.VoucherState),
+            total: integer(),
+            user: lazy(models.User),
+            partes: array(lazy(models.VoucherPart)),
+        }),
+
+    VoucherPart: () =>
+        object({
+            id: integer(),
+            partition_part: lazy(models.PartitionPart),
+            state: lazy(models.VoucherPartState),
+            pago_data: union([
+                object({
+                    filial: optional(lazy(models.Filial)),
+                }),
+                object({
+                    cuenta: optional(lazy(models.Cuenta)),
+                    direccion: optional(lazy(models.Direccion)),
+                }),
+            ]),
+            factura: object({
+                uri: optional(nullable(string())),
+                state: optional(lazy(models.FacturaState)),
+                waiting_facturation: optional(boolean()),
+            }),
+        }),
+
+    Filial: () =>
+        object({
+            id: integer(),
+            nombre: string(),
+            matriz_id: integer(),
+            rut: string(),
+        }),
+
+    Cuenta: () =>
+        object({
+            id: integer(),
+            banco: string(),
+            tipo: string(),
+            rut: string(),
+        }),
+
+    Direccion: () =>
+        object({
+            id: integer(),
+            nombre: string(),
+            direccion: string(),
+        }),
+
+    FacturaState: () => enums(['No Confirmada', 'Confirmada', 'Emitida']),
+
+    PartitionPart: () =>
+        object({
+            id: integer(),
+            total: integer(),
+            items: array(lazy(models.PartitionPartItem)),
+        }),
+
+    VoucherPartState: () =>
+        enums([
+            'Pendiente/Facturación Requerida',
+            'Pagado/Facturación Requerida',
+            'Pendiente',
+            'Pagado',
+            'Anulado',
+        ]),
+
+    VoucherState: () =>
+        enums([
+            'Pendiente/Facturación Requerida',
+            'Pagado/Facturación Requerida',
+            'Pendiente',
+            'Pagado',
+            'Anulado',
+        ]),
+
+    PartitionPartItem: () =>
+        object({
+            id: integer(),
+            monto: integer(),
+            data: string(),
+        }),
+
+    User: () =>
+        object({
+            id: integer(),
+            rut: string(),
+            nombre: string(),
+            facturacion_anticipada: boolean(),
+        }),
+};
+
+const morosidades = server.endpoint.getAll(
+    'documentos/morosidades',
+    array(models.Morosidad())
+);
+
+const voucherPagination = server.endpoint.getAll(
+    'voucher/persona',
+    models.VoucherPagination()
 );
 
 const login = server.endpoint.post<
@@ -47,7 +156,7 @@ const login = server.endpoint.post<
     'login/persona',
     object({
         token: string(),
-        id: number(),
+        id: integer(),
     })
 );
 
@@ -56,6 +165,8 @@ const App = () => {
         morosidades.fetch().then(console.log);
 
         login.fetch({ rut: 'blah' }).then(console.log);
+
+        voucherPagination.fetch().then(console.log);
     }, []);
 
     return (
--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,18 @@
+import { coerce, instance, string, Struct } from 'superstruct';
+import { Decimal } from 'decimal.js';
+import { LocalDate } from 'js-joda';
+
 /** The subset of HTTP methods used for CRUD operations. */
 export type CrudMethod = 'POST' | 'GET' | 'PUT' | 'PATCH' | 'DELETE';
+
+/** A SuperStruct custom type that coerces a string into a `Decimal` from `decimal.js`. */
+export const decimal = () =>
+    coerce(instance(Decimal), string(), s => new Decimal(s));
+
+/** A SuperStruct custom type that coerces a string into a `LocalDate` from `js-joda`. */
+export const localDate = (): Struct<LocalDate, null> /* [2] */ =>
+    coerce(instance(LocalDate as any /* [1] */), string(), s =>
+        LocalDate.parse(s)
+    );
+/* [1]: LocalDate constructor is private so we have to do this.
+ * [2]: LocalDate was casted to any in [1] so we tell TypeScript the correct type. */
