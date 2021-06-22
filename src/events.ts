3df2c4ddb4c8fdaff39b7e6a4514be8a914ab73e
Robinson Castro F <r.a.c.f.28@gmail.com>
2021-06-22 09:46:53 -0400

    add FetchError event to WindowEventMap

--- src/events.ts
+++ src/events.ts
@@ -1,3 +1,14 @@
+import { FetchError } from './FetchError';
+
 export const events = {
-    FETCH_ERROR: 'async-stuff::FetchError',
-};
+    FETCH_ERROR: '@tdc-cl/async-stuff::FetchError',
+} as const;
+
+/** A workaround for https://github.com/microsoft/TypeScript/issues/28357.
+ * It also makes custom event types to show up in IDE auto-complete. */
+declare global {
+    // noinspection JSUnusedGlobalSymbols
+    interface WindowEventMap {
+        [events.FETCH_ERROR]: CustomEvent<FetchError>;
+    }
+}
